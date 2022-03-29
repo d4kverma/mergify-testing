37aff0aa29ad3a9782abc3b56883f459973802c0
anmol-porall <anmol.nagpal@porall.com>
2022-03-29 17:36:17 +0530

    mergify modified

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@
 #Module      : label
 #Description : This terraform module is designed to generate consistent label names and
 #              tags for resources. You can use terraform-labels to implement a strict
-#              naming convention.
+#              naming convention..
 module "labels" {
   source  = "clouddrove/labels/aws"
   version = "0.15.0"
